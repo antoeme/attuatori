44563b97c370b600fe4e2a752f10d6809535782b
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-08-23 17:32:14 +0200

    aggiunto tutto in un unico file

--- app.py
+++ app.py
@@ -2,9 +2,43 @@
 import json
 from flask import Flask,jsonify
 from requests import request
-import module_relay as mt
+import requests
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+from urllib import response
+from bs4 import BeautifulSoup
+from requests.auth import HTTPBasicAuth as aut
+from os import getenv
+
+
+url_relays = getenv("url_relays") or "http://10.10.10.82/relays.cgi"
+url_set_relay = getenv("url_set_relay") or "http://10.10.10.82/relays.cgi?relay="  #bisogna appendere "1" o "2" per decidere quale relay cambiare
+# username = getenv("USERNAME") or "admin"
+# password = getenv("PASSWORD") or "dtl4b1tc2022!"
+username = "admin"
+password = "dtl4b1tc2022!"
+
+def get_stat():
+    result = requests.get(url_relays, auth=aut(username,password))   
+    doc = BeautifulSoup(result.text,"html.parser")
+
+    tags = doc.find_all("p")
+
+    s = str(tags[0].string) #estrae contenuto tags <p> .. <\p>
+    
+    l= s.split()   #divide la stringa in lista 
+    l.pop(0)    #elimino primo elemento 
+    status = {}
+
+    for i in range(2):
+        status["Relay"+str(i+1)] = int(l[i])
+    return(status)
+
+def set_stat(r):
+    url = url_set_relay + str(r)
+    result = requests.get(url,auth=aut(username,password)) 
+    return "cambiato stato relay " + str(r)
+
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
@@ -19,14 +53,12 @@ def verify_password(username, password):
     if username in users and check_password_hash(users.get(username), password):
         return username
 
-@app.route('/prova')
-def helloworld():
-    return jsonify({"about": " Helloworld !"})
+
 
 @app.route('/status', methods=['GET'] )   #rotta per la get delle temperature
 @auth.login_required
 def get_status():
-    json_temps = json.dumps(mt.get_stat())   # dump della lista delle temperature presa in module_temp.py
+    json_temps = json.dumps(get_stat())   # dump della lista delle temperature presa in module_temp.py
     return json_temps
 
 @app.route('/set_relay/<int:id_relay>', methods=[ 'GET','POST'])
@@ -35,7 +67,7 @@ def set_r(id_relay):
     if(id_relay != 1) and (id_relay != 2):  #controllare nel caso in cui si inserisce un carattere
         return ("Errore: ID del relay errato")
     else:
-        json_r = json.dumps(mt.set_stat(id_relay))
+        json_r = json.dumps(set_stat(id_relay))
         return json_r
 
 if __name__ == '__main__':
